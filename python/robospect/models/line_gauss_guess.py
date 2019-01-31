00eba154d1e649fbf65db93b426fc880ece18622
Christopher Waters <czw@princeton.edu>
2019-01-30 18:14:45 -0600

    Incomplete line_pre implementation.

--- python/robospect/models/line_gauss_guess.py
+++ python/robospect/models/line_gauss_guess.py
@@ -24,16 +24,60 @@ import robospect.lines as lines
 __all__ = ['line_gauss_guess']
 
 class line_gauss_guess(spectra.spectrum):
+    range = 25.0
 
-    # center
+    def _centroid(self, X, Y):
+        V = 0.0
+        W = 0.0
+        for vX, vY in zip(X, Y):
+            V += vX * vY
+            W += vY
+        if W == 0.0:
+            raise RuntimeError("line_gauss_guess.centroid: division by zero")
+        return V/W
 
-    # fwhm
+    def _eval_interpolant_fraction(self, central_index, value):
+        pass
+    def fit_initial(self):
+        for line in L:
+            start = np.searchsorted(self.x, line.x0 - range, side='left')
+            center= np.searchsorted(self.x, line.x0, side='right')
+            end   = np.searchsorted(self.x, line.x0 + range, side='right')
 
-    # fwqm
+            m = self._centroid(self.x[center-2:center+2],
+                               self.y[center-2:center+2] -
+                               self.continuum[center-2:center+2])
+            F = abs(self.y[center] - self.continuum[center])
 
-    # fw3m
+            subX = self.x[start:end]
+            subY = abs(self.y[start:end] - self.continuum[start:end])
 
-    # sigma
+            prior = F + 1
+            for dx in range(center, start):
+                if subY[dx] < prior and subY[dx] > 0.25 F:
+                    prior = subY[dx]
+                else:
+                    start = dx - 1
+                    break
+            prior = F + 1
+            for dx in range(center, end):
+                if subY[dx] < prior and subY[dx] > 0.25 F:
+                    prior = subY[dx]
+                else:
+                    end = dx + 1
+                    break
 
-    # flux
-    pass
+            subX = self.x[start:center]
+            subY = abs(self.y[start:center] - self.continuum[start:center])
+            hwhm1, hwqm1, hw3qm1 = np.interp([0.25 * F, 0.5 * F, 0.75 * F],
+                                             subY, subX)
+
+            subX = self.x[center:end]
+            subY = abs(self.y[center:end] - self.continuum[center:end])
+            hwhm2, hwqm2, hw3qm2 = np.interp([0.25 * F, 0.5 * F, 0.75 * F],
+                                             subY, subX)
+
+        # sigma
+
+        # flux
+        pass
