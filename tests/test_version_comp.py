02eef6220f50e288d8e4b1f63faf539d9d31dda5
Christopher Waters <czw@princeton.edu>
2019-05-21 19:07:16 -0400

    Add initial line parameter comparison plots.  I guess I know what the next step is. :(

--- tests/test_version_comp.py
+++ tests/test_version_comp.py
@@ -35,10 +35,10 @@ def read_lines(path_base):
         for entry in f:
             if not entry.startswith("#"):
                 t = entry.split()
-                L = RS.line(t[0], Nparam=4, comment=t[17], flags=t[15], blend=t[16])
-                L.Q = (t[1], t[2], t[3], t[4])
-                L.dQ = (t[5], t[6], t[7], t[8])
-                L.pQ = (t[9], t[10], t[11], 0.0)
+                L = RS.line(float(t[0]), Nparam=4, comment=t[17], flags=t[15], blend=t[16])
+                L.Q = (float(t[1]), float(t[2]), float(t[3]), float(t[4]))
+                L.dQ = (float(t[5]), float(t[6]), float(t[7]), float(t[8]))
+                L.pQ = (float(t[9]), float(t[10]), float(t[11]), 0.0)
                 lines.append(L)
     return lines
 
@@ -51,6 +51,56 @@ def run_current(path_base, *args):
     S = C.read_spectrum()
     S.fit()
     return S.x, S.y, S.error, S.continuum, S.lines, S.L
+
+def lines_compare(plot_key, pre_key, l1, l2):
+    if plot_key is None:
+        plot_key = ""
+
+    l1.sort(key=RS.sortLines)
+    l2.sort(key=RS.sortLines)
+
+    x  = []
+    dm = []
+    ds = []
+    dF = []
+
+    j = 0
+    R = 1e-3
+    for i, ll1 in enumerate(l1):
+        R = 1e-3
+        jt = -1
+        for j, ll2 in enumerate(l2):
+            # Yes, but I'm hungry, so I'm sucking up the O(N*M) for today.
+            Rt = np.abs(float(ll1.x0) - float(ll2.x0))
+            if Rt < R:
+                R = Rt
+                jt = j
+        if jt != -1:
+            ll2 = l2[jt]
+
+            x.append(ll1.x0)
+
+            if pre_key is True:
+                dm.append(ll1.pQ[0] - ll2.pQ[0])
+                ds.append(ll1.pQ[1] - ll2.pQ[2])
+                dF.append(ll1.pQ[2] - ll2.pQ[2])
+            else:
+                dm.append(ll1.Q[0] - ll2.Q[0])
+                ds.append(ll1.Q[1] - ll2.Q[2])
+                dF.append(ll1.Q[2] - ll2.Q[2])
+    # Close matching loop
+    plt.xlim(x[0], x[-1])
+    plt.ylim(-0.2, 0.2)
+    plt.xlabel("wavelength")
+    plt.ylabel(f"ref - fit")
+    plt.title(f"line comparison")
+
+    plt.plot(x, dm, 'ro', ms=5, label="mean")
+    plt.plot(x, ds, 'bs', ms=5, label="sigma")
+    plt.plot(x, dF, 'g^', ms=5, label="flux")
+    plt.legend(loc="upper left")
+    plt.savefig(f"/tmp/test_{plot_key}_lineParam.png")
+    plt.clf()
 
 
 def spect_compare(plot_key, path_base, *args):
@@ -104,6 +154,8 @@ def spect_compare(plot_key, path_base, *args):
                     np.min(compare), np.max(compare),
                     P[0], P[1], P[2], P[3], P[4]))
     outF.write("LINES:  %d %d\n" % (len(file_Lines), len(current_Lines)))
+    lines_compare(plot_key, True, file_Lines, current_Lines)
+
 
 def test_blue():
     spect_compare("blue", f"{TestDir}/v2.14run/blue.out_iter1")
